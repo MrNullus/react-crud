96903cf4f171075e90665fb859a1f865bb00cc81
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-01-07 00:08:25 -0300

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,31 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ values, setValues ] = useState();
+
+  const handleChangeValues = ( value ) => {
+    console.log("NAME VALUE ==> ", value.target.name);
+    console.log("VALUE ==> ", value.target.value);
+
+    let input = { 
+      name: value.target.name, 
+      value: value.target.value 
+    };
+
+    setValues(( preValues ) => ({
+      ...preValues,
+      [input.name]: input.value
+    }));
+  }
+
+  const handleClickButton = () => {
+    console.log("VALUES ==> ", values);
+  }
 
   return (
-    <div className="app-container">
-      <div className="register-container">
-        <h1>Scrim Shop</h1>
+    <div className="app--container">
+      <div className="register--container">
+        <h1 className="register--title">Scrim Shop</h1>
 
         <input 
           className="register--input"
@@ -14,6 +34,7 @@ function App() {
           name="name" 
           id="name" 
           placeholder="Name"
+          onChange={handleChangeValues}
         />
         <input 
           className="register--input"
@@ -21,6 +42,7 @@ function App() {
           name="cost" 
           id="cost" 
           placeholder="Preço"
+          onChange={handleChangeValues}
         />
         <input 
           className="register--input"
@@ -28,9 +50,13 @@ function App() {
           name="category" 
           id="category" 
           placeholder="Categoria"
+          onChange={handleChangeValues}
         />
 
-        <button>
+        <button
+          className="register--button"
+          onClick={handleClickButton}
+        >
           Cadastrar
         </button>
       </div>
