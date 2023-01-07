c6d770c1fb49b324788541f3b8d2f8dd07ccb188
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-01-06 22:44:41 -0300

    changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,8 +4,36 @@ import './App.css';
 function App() {
 
   return (
-    <div className="App">
-      
+    <div className="app-container">
+      <div className="register-container">
+        <h1>Scrim Shop</h1>
+
+        <input 
+          className="register--input"
+          type="text" 
+          name="name" 
+          id="name" 
+          placeholder="Name"
+        />
+        <input 
+          className="register--input"
+          type="text" 
+          name="cost" 
+          id="cost" 
+          placeholder="Preço"
+        />
+        <input 
+          className="register--input"
+          type="text" 
+          name="category" 
+          id="category" 
+          placeholder="Categoria"
+        />
+
+        <button>
+          Cadastrar
+        </button>
+      </div>
     </div>
   );
 }
