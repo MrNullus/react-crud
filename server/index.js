dececc31857340f7f971467fcb2f93f55e3c79db
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-01-07 03:26:18 -0300

    server: changes 🚀

--- server/index.js
+++ server/index.js
@@ -1,10 +1,42 @@
 const express = require("express");
+const mysql = require("mysql");
+
+// iniciando o express
 const app = express();
+// configurando banco de dados
+const db = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: "", 
+	database: "crudgames"
+});
+db.connect();
+
 
 // req -> tudo que enviar para a URL
 // res -> todo resultado vinido da URL
-app.get('/', ( req, res ) => {
-	res.send("Hello World!!!");
+app.get('/games', ( req, res ) => {
+	
+	// let SQL = `
+  // INSERT INTO games
+  // 	( name, cost, category ) 
+  // VALUES 
+  // 	( 'Far Cry 5', '120', 'Ação' )
+	// `;
+
+	let SQL = `
+  SELECT 
+  	*
+  FROM 
+  	games
+	`;
+
+	db.query(SQL, async ( err, res ) => {
+		console.log("ERROR ====> ", err);
+		console.log("RES   ====> ", JSON.stringify(res));
+		console.log("Foi");
+	});
+
 });
 
 app.listen(3001, () => {
