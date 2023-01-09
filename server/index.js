7d0242e8ab83cec896e383067a146b0a821514cf
dev-gustavo-henrique <gustavojs417@gmail.com>
2023-01-08 22:42:30 -0300

    changes

--- server/index.js
+++ server/index.js
@@ -1,44 +1,78 @@
 const express = require("express");
-const mysql = require("mysql");
-
-// iniciando o express
 const app = express();
-// configurando banco de dados
-const db = mysql.createConnection({
-	host: "localhost",
-	user: "root",
-	password: "", 
-	database: "crudgames"
+const mysql = require("mysql");
+const cors = require("cors");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "crudgames",
 });
-db.connect();
 
+app.use(express.json());
+app.use(cors());
 
-// req -> tudo que enviar para a URL
-// res -> todo resultado vinido da URL
-app.get('/games', ( req, res ) => {
-	
-	// let SQL = `
-  // INSERT INTO games
-  // 	( name, cost, category ) 
-  // VALUES 
-  // 	( 'Far Cry 5', '120', 'Ação' )
-	// `;
+app.post("/register", (req, res) => {
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
 
-	let SQL = `
-  SELECT 
-  	*
-  FROM 
-  	games
-	`;
+  let mysql = "INSERT INTO games ( name, cost, category) VALUES (?, ?, ?)";
+  db.query(mysql, [name, cost, category], (err, result) => {
+    res.send(result);
+  });
+});
 
-	db.query(SQL, async ( err, res ) => {
-		console.log("ERROR ====> ", err);
-		console.log("RES   ====> ", JSON.stringify(res));
-		console.log("Foi");
-	});
+app.post("/search", (req, res) => {
+  const { name } = req.body;
+  const { cost } = req.body;
+  const { category } = req.body;
 
+  let mysql =
+    "SELECT * from games WHERE name = ? AND cost = ? AND category = ?";
+  db.query(mysql, [name, cost, category], (err, result) => {
+    if (err) res.send(err);
+    res.send(result);
+  });
+});
+
+app.get("/getCards", (req, res) => {
+  let mysql = "SELECT * FROM games";
+  db.query(mysql, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.put("/edit", (req, res) => {
+  const { id, name, cost, category } = req.body;
+
+  let mysql = "UPDATE games SET name = ?, cost = ?, category = ? WHERE game_id = ?";
+  db.query(mysql, [name, cost, category, id], (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+});
+
+app.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+  let mysql = "DELETE FROM games WHERE game_id = ?";
+  db.query(mysql, id, (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(result);
+    }
+  });
 });
 
 app.listen(3001, () => {
-	console.log("Rodando servidor...");
-})
+  console.log("rodando na porta 3001");
+});
